44b5ebc73962d515da40c6ce03703efa319aa601
svasenkov <aanher@gmail.com>
2023-02-17 22:09:34 +0300

    added TextBoxTests

---
+++ src/test/java/TextBoxTests.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+
+        $("#userName").setValue("Alex Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address 1");
+        $("#submit").click();
+
+        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
+                text("Some address 1"), text("Another address 1"));
+    }
+}
