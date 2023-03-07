b4f210bacbae1748c415ea7a616d58e338893f09
svasenkov <aanher@gmail.com>
2023-03-07 21:03:22 +0300

    added static testdata

---
+++ src/test/java/tests/RegistrationWithTestDataTests.java
@@ -0,0 +1,79 @@
+package tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static tests.TestData.*;
+
+public class RegistrationWithTestDataTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+//    String userName = "Alex",
+//            lastName = "Egorov",
+//            userEmail = "alex@egorov.com";
+
+//    static String userName,
+//            lastName,
+//            userEmail;
+
+    @BeforeEach
+    void prepareTestData() {
+//        userName = "Alex";
+//        lastName = "Egorov";
+//        userEmail = "alex@egorov.com";
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+//        String userName = "Alex",
+//                lastName = "Egorov",
+//                userEmail = "alex@egorov.com";
+
+        registrationPage.openPage()
+                .setFirstName(userName)
+                .setLastName(lastName)
+                .setEmail(userEmail)
+                .setGender("Other")
+                .setPhone("1234567890")
+                .setBirthDate("30", "July", "2008");
+
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        registrationPage.verifyResultsModalAppears()
+                .verifyResult("Student Name", userName + " " + lastName)
+                .verifyResult("Student Email", userEmail)
+                .verifyResult("Gender", "Other")
+                .verifyResult("Mobile", "1234567890")
+                .verifyResult("Date of Birth", "30 July,2008");
+//        registrationPage.registrationResultsModal.verifyResult("Student Name", userName + " Egorov");
+    }
+
+    @Test
+    void successfulRegistration1Test() {
+        String userName = "Alex";
+
+        registrationPage.openPage();
+
+        registrationPage.setFirstName(userName);
+        registrationPage.setLastName("Egorov");
+        registrationPage.setEmail("alex@egorov.com");
+        registrationPage.setGender("Other");
+        registrationPage.setPhone("1234567890");
+
+        $("#dateOfBirthInput").click();
+        // ...
+    }
+
+}
