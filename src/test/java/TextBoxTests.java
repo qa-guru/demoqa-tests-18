208382b5811801d116278a4e8b75a1e91f38cc9e
annashv <annashivrina@gmail.com>
2024-03-07 19:09:00 +0100

    Added test

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +11,21 @@ public class TextBoxTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        if ($(".fc-button-label").isDisplayed())
+            $(".fc-button-label").click();
+
+        $("#userName").setValue("Alex Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some address 1");
+        $("#permanentAddress").setValue("Another address 1");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
+                text("Some address 1"), text("Another address 1"));
     }
 }
